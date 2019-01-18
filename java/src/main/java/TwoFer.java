899a7ec36bc4f18ff230fcc71c875e8ba166172f
Stanly Lau <stanly_lau@tech.gov.sg>
2019-01-18 14:36:18 +0800

    Added java project

---
+++ java/src/main/java/TwoFer.java
@@ -0,0 +1,8 @@
+public class TwoFer {
+    public String say(String input) {
+        if(input == null)
+            return "One for " + input + ", one for me.";
+
+        return "One for you, one for me.";
+    }
+}
---
+++ java/src/test/java/TwoFerTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TwoFerTest {
+    @Test
+    public void test() {
+        assertEquals("hello", "hello");
+    }
+}
