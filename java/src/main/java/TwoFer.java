c0f5d078747b4de1cc6b14e5f983111d9a20238c
Stanly Lau <stanly@odd-e.com>
2019-09-12 17:23:11 +0800

    Upgraded gradle and fixed TwoFer

--- java/src/main/java/TwoFer.java
+++ java/src/main/java/TwoFer.java
@@ -1,6 +1,6 @@
 public class TwoFer {
     public String say(String input) {
-        if(input == null)
+        if(input != null)
             return "One for " + input + ", one for me.";
 
         return "One for you, one for me.";
